df3074376ab0369ba016f136e76ed878baed0bc9
letopeace <dumanaskerbek@gmail.com>
2025-04-11 22:23:46 +0500

    WASD

---
+++ TgJam_TurKing/Assets/Scripts/PlayerCamera.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour
+{
+    public GameObject cam;
+    public float yMin = -90, yMax = 90;
+
+    private float x, y;
+
+    private void Update()
+    {
+        Look();
+    }
+
+    private void Look()
+    {
+        x += Input.GetAxis("Mouse X");
+        y += Input.GetAxis("Mouse Y");
+
+        Vector3 camRot = cam.transform.eulerAngles;
+        camRot.x = Mathf.Clamp(-y, yMin, yMax);
+        cam.transform.eulerAngles = camRot;
+
+        Vector3 playerRot = transform.eulerAngles;
+        playerRot.y = x;
+        transform.eulerAngles = playerRot;
+    }
+}
--- TgJam_TurKing/Assets/Scripts/PlayerMovement.cs
+++ TgJam_TurKing/Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    public float speed = 10f;
+
+
+    private Rigidbody rb;
+
+
+    private void Awake()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        WASD();
+    }
+
+    private void WASD()
+    {
+
+
+
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        Vector3 dir = Vector3.ClampMagnitude((transform.right * h + transform.forward * v), 1f);
+        dir = dir * speed * Time.fixedDeltaTime;
+        dir.y = rb.velocity.y;
+
+        rb.velocity = dir;
     }
 }
