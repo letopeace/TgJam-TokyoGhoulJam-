90fb873218d91b259bd3a7ed4e465535f9f7a12f
rakimkz1 <89095897+rakimkz1@users.noreply.github.com>
2025-04-11 23:45:14 +0500

    3

--- TgJam_TurKing/Assets/Scripts/PlayerMovement.cs
+++ TgJam_TurKing/Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,9 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float speed;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    private Rigidbody rb;
+
+
 }
