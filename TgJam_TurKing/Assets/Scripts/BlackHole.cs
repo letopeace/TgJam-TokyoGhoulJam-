bdf2a662bc07ef5136272081dc2cab5d55246a3f
rakimkz1 <89095897+rakimkz1@users.noreply.github.com>
2025-04-12 18:40:49 +0500

    22

--- TgJam_TurKing/Assets/Scripts/BlackHole.cs
+++ TgJam_TurKing/Assets/Scripts/BlackHole.cs
@@ -9,12 +9,13 @@ public class BlackHole : MonoBehaviour
     public float holeForce;
     public float speed;
     public float lifeTime;
+    public float exploinDamage;
+    public GameObject exploinParticals;
     private void Update()
     {
         transform.Translate(transform.forward * speed * Time.deltaTime);
 
         Collider[] coliders = Physics.OverlapSphere(transform.position, radius);
-        Collider[] inColiders = Physics.OverlapSphere(transform.position, internalRaduis);
 
         for(int i = 0; i < coliders.Length; i++)
         {
@@ -24,16 +25,38 @@ public class BlackHole : MonoBehaviour
                 coliders[i].GetComponent<BaseEnemy>().staned = true;
             }
         }
-        for(int i =0; i<inColiders.Length;i++)
+        if (lifeTime < 0f) Exploin();
+        lifeTime -= Time.deltaTime;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject != null && other.gameObject.GetComponent<BaseEnemy>() != null)
         {
-            if (inColiders[i] != null && inColiders[i].gameObject.GetComponent<BaseEnemy>() != null)
+            other.gameObject.GetComponent<BaseEnemy>().Death();
+        }
+        else if (other.gameObject != null && other.gameObject.GetComponent<PlayerMovement>() == null)
+        {
+            Exploin();
+        }
+    }
+
+    void Exploin()
+    {
+        Collider[] coliders = Physics.OverlapSphere(transform.position, radius);
+        for(int i=0;i<coliders.Length;i++)
+        {
+            if (coliders[i] != null && coliders[i].gameObject.GetComponent<BaseEnemy>() != null)
             {
-                inColiders[i].GetComponent<BaseEnemy>().Death();
+                coliders[i].gameObject.GetComponent<BaseEnemy>().Damaged((int)(exploinDamage * (1f-(Vector3.Distance(transform.position, coliders[i].transform.position)/radius))));
             }
         }
+        Instantiate(exploinParticals, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+
     }
     private void OnDrawGizmos()
     {
         Gizmos.DrawWireSphere(transform.position, radius);
     }
+
 }
